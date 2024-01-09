de61b0ee589e61df4ab1b75536a71ffebee23374
GrizzlyH <58311879+GrizzlyH@users.noreply.github.com>
2024-01-09 15:02:10 +0200

    Update main.py

--- Video 1/main.py
+++ Video 1/main.py
@@ -1,1 +1,39 @@
+import pygame
 
+
+class BomberMan:
+    def __init__(self):
+        pygame.init()   #  Init pygame module
+
+        self.screen = pygame.display.set_mode((1280, 892))
+        self.FPS = pygame.time.Clock()
+
+        self.run = True
+
+
+    def input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.run = False
+
+
+    def update(self):
+        self.FPS.tick(60)   #  The speed at which pygame will update each cycle
+
+
+    def draw(self, window):
+        window.fill((0, 0, 0))  #  Fill the screen with a black colour
+        pygame.display.update()
+
+
+    def rungame(self):
+        while self.run:
+            self.input()
+            self.update()
+            self.draw(self.screen)
+
+
+if __name__=="__main__":
+    game = BomberMan()
+    game.rungame()
+    pygame.quit()
