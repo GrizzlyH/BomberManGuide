8a89c926f696f5ecca53abd42b730646e6ef9a36
Harry <harry.b21@gmail.com>
2024-01-22 10:55:33 +0200

    Update Tutorial

--- gamesettings.py
+++ gamesettings.py
@@ -5,5 +5,18 @@ SCREENHEIGHT = 892
 #  Game Frames per Second
 FPS = 60
 
+#  Game Matrix
+SIZE = 64
+
 #  Colours
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+#  Sprite Coordinates
+PLAYER = {"walk_left": [(0, 1), (0, 0), (0, 2)],
+          "walk_down": [(0, 4), (0, 3), (0, 5)],
+          "walk_right": [(0, 7), (0, 6), (0, 8)],
+          "walk_up": [(0, 10), (0, 9), (0, 11)],
+          "dead_anim": [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6)]}
