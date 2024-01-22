2ff31a87bd129ffc4f33956f583541e085e5553d
Harry <harry.b21@gmail.com>
2024-01-22 13:41:42 +0200

    Update Tutorial

--- gamesettings.py
+++ gamesettings.py
@@ -7,6 +7,8 @@ FPS = 60
 
 #  Game Matrix
 SIZE = 64
+ROWS = 12
+COLS = 30
 
 #  Colours
 BLACK = (0, 0, 0)
@@ -20,3 +22,6 @@ PLAYER = {"walk_left": [(0, 1), (0, 0), (0, 2)],
           "walk_right": [(0, 7), (0, 6), (0, 8)],
           "walk_up": [(0, 10), (0, 9), (0, 11)],
           "dead_anim": [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6)]}
+HARD_BLOCK = {"hard_block": [(1, 10)]}
+SOFT_BLOCK = {"soft_block": [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6)]}
+BACKGROUND = {"background": [(2, 11)]}
